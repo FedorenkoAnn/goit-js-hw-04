4e020b10e6412dc0a5477241d03fc5e5a4b5461f
FedorenkoAnn <fedorenkoann98@gmail.com>
2023-12-01 15:07:23 +0200

    HomeWork

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,19 @@
-function slugify(title) {
-  return title.split(' ').join('-').toLowerCase();
+function isEnoughCapacity(products, containerSize) {
+  let totalQuantity = 0;
+  for (const quantity of Object.values(products)) {
+  totalQuantity += quantity;
 }
-
-console.log(slugify('Arrays for begginers')); // "arrays-for-begginers"
-console.log(slugify('English for developer')); // "english-for-developer"
-console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
-console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
+return totalQuantity <= containerSize;
+}
+console.log(
+isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+);
+console.log(
+isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+);
+console.log(
+isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+);
+console.log(
+isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+);
